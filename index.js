07ff2db8a0d26e8fe877f5b70153c626e657ac65
RedaDamene <damene.reda4@gmail.com>
2022-10-20 23:10:03 +0200

    Affichage des messages en fonction du résultat

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const calcul = document.querySelector('#calcul');
 let result = document.getElementById('result');
 
-var t1 = "Je ne sait pas"
-var t2 = "Je ne sait pas"
-var t3 = "Je ne sait pas"
-var t4 = "Je ne sait pas"
-var t5 = "Je ne sait pas"
-var t6 = "Je ne sait pas"
-var t7 = "Je ne sait pas"
+var t1 = "Insuffisance pondérante"
+var t2 = "Poids normal"
+var t3 = "Surpoids"
+var t4 = "Obésité"
+
 
 calcul.addEventListener('click' , (e) => {
+    e.preventDefault();
     var size = document.getElementById('size').value/100;
     var weight = document.getElementById('weight').value;
     let imc = weight/size**2;
 
-    result.innerHTML = `
-        <h3>Voici votre IMC : ${imc}</h3>
-        <p>${t1}</p>
-    `
-
-    e.preventDefault();
+    if (size == 0 || weight == 0){
+        result.innerHTML = `
+            <h3>Formulaire invalide</h3>
+            <p>Veuillez remplir les champs</p>
+        `
+    }else{
+        if(imc < 18.5){
+            result.innerHTML = `
+            <h3>Voici votre IMC : ${imc}</h3>
+            <p>${t1}</p>
+        `
+        }else if(imc > 18.5 && imc < 24.9){
+            result.innerHTML = `
+            <h3>Voici votre IMC : ${imc}</h3>
+            <p>${t2}</p>
+        `
+        }else if(imc < 25 && imc < 29.9){
+            result.innerHTML = `
+            <h3>Voici votre IMC : ${imc}</h3>
+            <p>${t3}</p>
+        `
+        }else{
+            result.innerHTML = `
+            <h3>Voici votre IMC : ${imc}</h3>
+            <p>${t4}</p>
+        `
+        }
+    }
 })
